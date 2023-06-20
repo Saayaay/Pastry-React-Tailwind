b5214ebe078856f4d8f6e33c5bac77d7147c5034
Sayadyna <sayadyna.m@gmail.com>
2023-06-20 19:11:52 +0200

    Create Form

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -32,7 +32,30 @@ const Search = () => {
                 </div>
             </div>
         </div>
-        <div>right</div>
+
+        <div>
+            <div className='border text-center'>
+                <p className='pt-2'>-10% SUR NOS GATEAUX D'ANNIVERSAIRE</p>
+                <p className='py-4'>12 HEURES AVANT ECOULEMENT DE L'OFFRE</p>
+                <p className='bg-orange-400 text-gray-50 py-2'>RESERVER MAINTENANT</p>
+            </div>
+            <form className='w-full'>
+                <div className='flex flex-col my-2'>
+                    <label>GÂTEAUX</label>
+                    <select className='border rouonded-md p-2'>
+                        <option>Fraisier à la vie à la mort</option>
+                        <option>Framboisier c'est de la bombe bébé</option>
+                        <option>Cake à la pistache, moustache !</option>
+                        <option>Paris-Brest géant comme on en fait plus</option>
+                    </select>
+                </div>
+                <div className='flex flex-col my-4'>
+                    <label>Date </label>
+                    <input className='border rounded-md p-2'type="date" />
+                </div>
+                <button className='w-full my-4'>Commander</button>
+            </form>
+        </div>
     </div>
   )
 }
